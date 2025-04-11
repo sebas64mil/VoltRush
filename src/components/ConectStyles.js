cee1eba7569944f3ab03b670d6e41b102fe62e7e
sebas64mil <162932132+sebas64mil@users.noreply.github.com>
2025-04-11 17:41:51 -0500

    change

--- src/components/ConectStyles.js
+++ src/components/ConectStyles.js
@@ -1,28 +1,29 @@
-fetch("../../Tokens.json")
-    .then(res => res.json())
-    .then(styles => {
-        const root = document.documentElement;
+// fetch("../../Tokens.json")
+//     .then(res => res.json())
+//     .then(styles => {
+//         const root = document.documentElement;
 
-        // 1. Access the palette to apply (Palette 2)
-        const collection = styles["GlobalSettingsColors/Mode 1"];
-        const palette = collection["Paleta 2"];
+//         // 1. Access the palette to apply (Palette 2)
+//         const collection = styles["GlobalSettingsColors/Mode 1"];
+//         //Recorres todas las colecciones de colores y seleccionas la que quieres aplicar
+//         const palette = collection["Paleta 2"];
 
-        // 2. Access the base colors (tailwind colors)
-        const tailwindColors = styles["Tailwind Colors/Mode 1"];
+//         // 2. Access the base colors (tailwind colors)
+//         const tailwindColors = styles["Tailwind Colors/Mode 1"];
 
-        for (const color in palette) {
-            const value = palette[color].value; // example: "{emerald.50}"
-            const cleanToken = value.replace(/[{}]/g, ""); // "emerald.50"
+//         for (const color in palette) {
+//             const value = palette[color].value; // example: "{emerald.50}"
+//             const cleanToken = value.replace(/[{}]/g, ""); // "emerald.50"
 
-            const [group, shade] = cleanToken.split("."); // ["emerald", "50"]
+//             const [group, shade] = cleanToken.split("."); // ["emerald", "50"]
 
-            const actualColor = tailwindColors?.[group]?.[shade]?.value;
+//             const actualColor = tailwindColors?.[group]?.[shade]?.value;
 
-            if (actualColor) {
-                root.style.setProperty(`--${color}`, actualColor);
-                console.log(`--${color}: ${actualColor}`);
-            } else {
-                console.warn(`Color not found for ${group}.${shade}`);
-            }
-        }
-    });
+//             if (actualColor) {
+//                 root.style.setProperty(`--${color}`, actualColor);
+//                 console.log(`--${color}: ${actualColor}`);
+//             } else {
+//                 console.warn(`Color not found for ${group}.${shade}`);
+//             }
+//         }
+//     });
