43c5de0acf36f229c01a28becc82a9addd1d07cd
sebas64mil <162932132+sebas64mil@users.noreply.github.com>
2025-04-11 21:05:14 -0500

    añadiendo mas librerias

--- src/components/Settingsproject/SetStyles.js
+++ src/components/Settingsproject/SetStyles.js
@@ -1,44 +1,93 @@
-// fetch("../../Tokens.json")
-//     .then(res => res.json())
-//     .then(styles => {
-//         const root = document.documentElement;
-
-//         // 1. Access the palette to apply (Palette 2)
-//         const collection = styles["GlobalSettingsColors/Mode 1"];
-//         //Recorres todas las colecciones de colores y seleccionas la que quieres aplicar
-//         const palette = collection["Paleta 2"];
-
-//         // 2. Access the base colors (tailwind colors)
-//         const tailwindColors = styles["Tailwind Colors/Mode 1"];
-
-//         for (const color in palette) {
-//             const value = palette[color].value; // example: "{emerald.50}"
-//             const cleanToken = value.replace(/[{}]/g, ""); // "emerald.50"
-
-//             const [group, shade] = cleanToken.split("."); // ["emerald", "50"]
-
-//             const actualColor = tailwindColors?.[group]?.[shade]?.value;
-
-//             if (actualColor) {
-//                 root.style.setProperty(`--${color}`, actualColor);
-//                 console.log(`--${color}: ${actualColor}`);
-//             } else {
-//                 console.warn(`Color not found for ${group}.${shade}`);
-//             }
-//         }
-//     });
-
 import { GetJson } from './ConectStyles.js';
+
 class LoadRootStyles {
     constructor() {
-        this.tailwind = GetJson("../public/lybraries/static/tailwind.json", "Tailwind Colors/Mode 1");
-        this.pallets = GetJson("../public/lybraries/variables/colors.json", "ColorsPage/Mode 1");
-        this.spaced = GetJson("../public/lybraries/variables/GlobalSpacing.json", "Spacing/light");
-        this.SettingDesktop = GetJson("../public/lybraries/variables/SettingsScreen.json", "Settingspage/desktop");
-        this.SettingMobile = GetJson("../public/lybraries/variables/SettingsScreen.json", "Settingspage/mobile");
+        this.root = document.documentElement;
+    }
+    async init() {
+        this.tailwind = await GetJson("../public/lybraries/static/tailwind.json", "Tailwind Colors/Mode 1");
+        this.pallets = await GetJson("../public/lybraries/variables/colors.json", "ColorsPage/Mode 1");
+        this.spaced = await GetJson("../public/lybraries/variables/GlobalSpacing.json", "Spacing/light");
+        this.SettingScreen = await GetJson("../public/lybraries/variables/SettingsScreen.json", "SettingsScreen");
+        this.typography = await GetJson("../public/lybraries/variables/Typography.json", "Typography");
+    }
+    async Loadpallet() {
+        
+        for (const namePallete in this.pallets) {
+
+            const palette = this.pallets[namePallete];
+            
+            for (const color in palette) {   
+
+            const value = palette[color]?.value;
+            const cleanToken = value.replace(/[{}]/g, "");
+            const [group, shade] = cleanToken.split(".");
+            const actualColor = this.tailwind?.[group]?.[shade]?.value;
+
+            if (actualColor) {
+                this.root.style.setProperty(`--${color}`, actualColor);
+                console.log(`--${color}: ${actualColor}`);
+            } else {
+                console.warn(`Color not found for ${group}.${shade}`);
+            }
+
+            }
+        }
     }
 
-    async Loadpallet(){
+    async LoadSpaced() {
+        for (const type in this.spaced) {
+        const TypeSpacing = this.spaced[type];
+        console.log(`${type}`);
+        
+        for(const spacing in TypeSpacing) {
+            const value = TypeSpacing[spacing]?.value;
+            console.log(`--${spacing}: ${value}`);
+            this.root.style.setProperty(`--${spacing}`, value);
+            
 
+        }
     }
 }
+
+    async LoadSettings() {
+    for(Screen in this.SettingScreen) {
+        const ScreenSettings = this.SettingScreen[Screen];
+        console.log(`${Screen}`);
+        
+        for(const Settings in ScreenSettings) {
+            const value = ScreenSettings[Settings]?.value;
+            console.log(`--${Settings}: ${value}`);
+            this.root.style.setProperty(`--${Settings}`, value);
+            
+
+        }
+    }
+}
+async LoadTypography() {
+    for (const type in this.typography) {
+        const TypeTypography = this.typography[type];
+        console.log(`${type}`);
+        
+        for (const typography in TypeTypography) {
+            let value = TypeTypography[typography]?.value;
+
+            // Verifica si el value es un número puro (sin letras, unidades, etc.)
+            if (!isNaN(value) && value.trim() !== "") {
+                value +="px"; // Agrega "px" si es un número puro;
+            }
+
+            console.log(`--${typography}:`, value);
+            this.root.style.setProperty(`--${typography}`, value);
+        }
+    }
+}
+
+}
+
+const LoadStyles = new LoadRootStyles();
+await LoadStyles.init();
+await LoadStyles.Loadpallet();
+await LoadStyles.LoadSpaced();
+await LoadStyles.LoadSettings();
+await LoadStyles.LoadTypography();
