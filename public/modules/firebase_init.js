beb077614e8565169d5e84cf1ea8d854afb9b472
juan <juandapacanhmail.com>
2025-04-24 17:07:18 -0500

    fire

--- public/modules/firebase_init.js
+++ public/modules/firebase_init.js
@@ -6,16 +6,23 @@ import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.1/firebase
 
 // Your web app's Firebase configuration
 
+// const firebaseConfig = {
+//   apiKey: "AIzaSyBBgoWaCd6APngiq3Jv-d4abmByqZyFxJ8",
+//   authDomain: "db-multimedia.firebaseapp.com",
+//   projectId: "db-multimedia",
+//   storageBucket: "db-multimedia.firebasestorage.app",
+//   messagingSenderId: "703694156223",
+//   appId: "1:703694156223:web:cd702f775e4417841db10a"
+// };
+
 const firebaseConfig = {
-  apiKey: "AIzaSyBBgoWaCd6APngiq3Jv-d4abmByqZyFxJ8",
-  authDomain: "db-multimedia.firebaseapp.com",
-  projectId: "db-multimedia",
-  storageBucket: "db-multimedia.firebasestorage.app",
-  messagingSenderId: "703694156223",
-  appId: "1:703694156223:web:cd702f775e4417841db10a"
+  apiKey: "AIzaSyANIYCtkrM7UcmFJK4AunyERlGKiHN8l1k",
+  authDomain: "voltrush-c1ee8.firebaseapp.com",
+  projectId: "voltrush-c1ee8",
+  storageBucket: "voltrush-c1ee8.firebasestorage.app",
+  messagingSenderId: "902027158636",
+  appId: "1:902027158636:web:18bcf5560098dd6f6fa449"
 };
-
-
 
 
 // Initialize Firebase
